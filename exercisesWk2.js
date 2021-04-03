5f7ce85ce71d834b7ae1fb07c602c98e4710248a
Cassie Anderson <cbp.smith@gmail.com>
2021-04-02 22:56:10 -0400

    Added more problems

--- exercisesWk2.js
+++ exercisesWk2.js
@@ -1,7 +1,6 @@
 function madLib(name, subject){
     return name+"'s favorite subject in school is "+subject+".";
 }
-
 madLib("Joe", "math");
 
 function tipAmount(billAmt, levelOfService) {
@@ -16,7 +15,6 @@ function tipAmount(billAmt, levelOfService) {
     tipAmt *= billAmt;
     return tipAmt;
 }
-
 tipAmount(100, 'poor');
 
 function totalAmount(billAmt, levelOfService) {
@@ -24,14 +22,72 @@ function totalAmount(billAmt, levelOfService) {
     var totalAmount = billAmt + tipAmt;
     return totalAmount;
 }
-
 totalAmount(100, 'fair');
 
-function printNumbers(startNum, endNum) {
+function printNumbersFor(startNum, endNum) {
     var i;
     for(i=startNum; i<=endNum; i++) {
         console.log(i);
     }
 }
+printNumbersFor(1, 10);
 
-printNumbers(1, 10);
+function printNumbersWhile(startNum, endNum) {
+    var i = startNum;
+    while(i<=endNum) {
+        console.log(i);
+        i++;
+    }
+}
+printNumbersWhile(1, 10);
+
+
+function printSquare(squareSz){
+    var length = squareSz;
+    for(count=0; count<length; count++){
+        var i = 0;
+        var line = "";
+        while(i<squareSz){
+            line = line + '*';
+            i++;
+        }
+        console.log(line);
+        line = "";
+    }
+}
+printSquare(5);
+
+//not working yet
+function printBox(w,h){
+    boxWidth(w);
+    boxHeight(h, w);
+    boxWidth(w);
+}
+printBox(6,4);
+
+//working
+function boxWidth(w){
+    var i = 0;
+    var line = "";
+    while(i<w){
+        line = line + '*';
+        i++;
+    }
+    console.log(line);
+    line = "";
+}
+//not working
+function boxHeight(h, w){
+    var height = h-2;
+    var blank = w-2;
+    console.log('* '+);
+
+}
+
+function printBanner(text){
+    bannerLength = text.length + 4;
+    boxWidth(bannerLength);
+    console.log('* '+text+' *');
+    boxWidth(bannerLength);
+}
+printBanner("hello");
